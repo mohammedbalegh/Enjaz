38c3e0a721075506d6d5033dba8f8df5e82ce7ac
mohammedbalegh <mohammed777balegh@gmail.com>
2020-12-03 23:43:10 +0200

    Added PrimaryBtn component

---
+++ Enjaz/CarouselCard.swift
@@ -0,0 +1,8 @@
+//
+//  CarouselCard.swift
+//  Enjaz
+//
+//  Created by mohammed balegh on 02/12/2020.
+//
+
+import Foundation
---
+++ Enjaz/Views/PrimaryBtn.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+class PrimaryBtn: UIButton {
+    
+    enum btnThemeType {
+        case blue
+        case white
+    }
+    
+    var label = ""
+    var theme: btnThemeType = .blue
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    init(label: String, theme: btnThemeType) {
+        super.init(frame: .zero)
+        self.label = label
+        self.theme = theme
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        
+        if theme == .blue {
+            self.backgroundColor = Colors.mainBTNColor
+            self.setTitleColor(.white, for: .normal)
+            self.layer.shadowColor = Colors.mainBTNColor?.cgColor
+        } else {
+            self.backgroundColor = .white
+            self.setTitleColor( .gray, for: .normal)
+            self.layer.shadowColor = UIColor.gray.cgColor
+        }
+        
+        self.setTitle(label, for: .normal)
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowRadius = 2.0
+        self.layer.cornerRadius = 14
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 10.0)
+
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
