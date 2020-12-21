201e47993ea231f3689d13ac22394b7b430bcd43
Mohammed Balegh <mohammed777balegh@gmail.com>
2020-12-21 15:28:31 +0200

    Finished additionTypeScreen.

---
+++ Enjaz/ViewControllers/Screens/AdditionTypeScreenVC.swift
@@ -0,0 +1,135 @@
+
+import UIKit
+
+class AdditionTypeScreenVC: UIViewController, AdditionTypeScreenCardDelegate {
+    
+    var chosenCard = 0
+    
+    lazy var taskCard: TypeCardBtn = {
+        let card = TypeCardBtn()
+        card.delegate = self
+        card.id = 0
+        card.typeImage.image = UIImage(named: "taskIcon")
+        card.typeTitle.text = "مهمة"
+        card.typeDescription.text = "ان كان لديك مهمة تريد انجازها خلال وقت معين"
+        return card
+    }()
+    
+    lazy var demahCard: TypeCardBtn = {
+        let card = TypeCardBtn()
+        card.delegate = self
+        card.id = 1
+        card.typeImage.image = UIImage(named: "demahIcon")
+        card.typeTitle.text = "ديمة"
+        card.typeDescription.text = "اذا كان لديك عادة متكررة خلال فترة معينة"
+        return card
+    }()
+    
+    lazy var achievementCard: TypeCardBtn = {
+        let card = TypeCardBtn()
+        card.delegate = self
+        card.id = 2
+        card.typeImage.image = UIImage(named: "achievementIcon")
+        card.typeTitle.text = "إنجاز"
+        card.typeDescription.text = "ان كان لديك أنجاز تريد تحقيقه خلال وقت معين"
+        return card
+    }()
+    
+    lazy var goalCard: TypeCardBtn = {
+        let card = TypeCardBtn()
+        card.delegate = self
+        card.id = 3
+        card.typeImage.image = UIImage(named: "goalIcon")
+        card.typeTitle.text = "هدف"
+        card.typeDescription.text = "ان كان لديك هدف في مجال معين تريد تحقيقه "
+        return card
+    }()
+    
+    lazy var stackTaskDemah: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [taskCard, demahCard])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.axis = .vertical
+        stackView.distribution  = .fillEqually
+        stackView.spacing = LayoutConstants.screenHeight * 0.042
+        
+        return stackView
+    }()
+    
+    lazy var stackAchievementGoal: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution  = .fillEqually
+        stackView.spacing = LayoutConstants.screenHeight * 0.042
+        
+        stackView.addArrangedSubview(achievementCard)
+        stackView.addArrangedSubview(goalCard)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    let saveBtn = PrimaryBtn(label: "حفظ", theme: .blue)
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .rootTabBarScreensBackgroundColor
+        
+        setupSubviews()
+    }
+    
+    func setupSubviews() {
+        setupStackTaskDemah()
+        setupStackAchievementGoal()
+        setupSaveBtn()
+    }
+    
+    func setupStackTaskDemah() {
+        view.addSubview(stackTaskDemah)
+        
+        let width = LayoutConstants.screenWidth * 0.4
+        
+        NSLayoutConstraint.activate([
+            stackTaskDemah.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: (LayoutConstants.screenWidth * 0.05)),
+            stackTaskDemah.widthAnchor.constraint(equalToConstant: width),
+            stackTaskDemah.heightAnchor.constraint(equalToConstant: width * 3),
+            stackTaskDemah.topAnchor.constraint(equalTo: view.topAnchor, constant: LayoutConstants.screenHeight * 0.123)
+        ])
+    }
+    
+    func setupStackAchievementGoal() {
+        view.addSubview(stackAchievementGoal)
+        
+        let width = LayoutConstants.screenWidth * 0.4
+        
+        NSLayoutConstraint.activate([
+            stackAchievementGoal.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -(LayoutConstants.screenWidth * 0.05)),
+            stackAchievementGoal.widthAnchor.constraint(equalToConstant: width),
+            stackAchievementGoal.heightAnchor.constraint(equalToConstant: width * 3),
+            stackAchievementGoal.topAnchor.constraint(equalTo: view.topAnchor, constant: LayoutConstants.screenHeight * 0.123)
+        ])
+    }
+    
+    func setupSaveBtn() {
+        view.addSubview(saveBtn)
+        
+        let width = LayoutConstants.screenWidth * 0.86
+        
+        NSLayoutConstraint.activate([
+            saveBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            saveBtn.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -(LayoutConstants.screenHeight * 0.04)),
+            saveBtn.widthAnchor.constraint(equalToConstant: width),
+            saveBtn.heightAnchor.constraint(equalToConstant: width * 0.182)
+        ])
+    }
+    
+    func onCardSelect(cardId selectedCardId: Int) {
+        taskCard.selectedId = selectedCardId
+        demahCard.selectedId = selectedCardId
+        goalCard.selectedId = selectedCardId
+        achievementCard.selectedId = selectedCardId
+    }
+    
+}
---
+++ Enjaz/Views/TypeCardBtn.swift
@@ -0,0 +1,147 @@
+
+import UIKit
+
+class TypeCardBtn: UIButton {
+
+    var id = 1
+    
+    var selectedId: Int = -1 {
+        didSet {
+            onSelectedIdChange()
+        }
+    }
+    
+    let typeImage: UIImageView = {
+        let image = UIImageView()
+        image.asCircle()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    let typeTitle: UILabel = {
+        let label = UILabel()
+        label.font = label.font.withSize(18)
+        label.textColor = UIColor(hex: 0x011942)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let typeDescription: UILabel = {
+        let label = UILabel()
+        label.font = label.font.withSize(12)
+        label.textColor = .darkGray
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let checkMark: UIImageView = {
+        let image = UIImageView()
+        image.asCircle()
+        image.image = UIImage(named: "checkMarkIcon")
+        image.isHidden = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    var delegate: AdditionTypeScreenCardDelegate?
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layer.cornerRadius = 10
+        self.layer.masksToBounds = true
+        
+        addTarget(self, action: #selector(onViewTap), for: .touchUpInside)
+        
+        self.layer.borderColor = UIColor.accentColor.cgColor
+        self.backgroundColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        setupSubviews()
+    }
+    
+    func setupSubviews() {
+        setupTypeImage()
+        setupTypeTitle()
+        setupTypeDescription()
+        setupCheckMark()
+    }
+    
+    func setupTypeImage() {
+        addSubview(typeImage)
+        
+        let size = self.frame.width * 0.457
+        
+        NSLayoutConstraint.activate([
+            typeImage.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            typeImage.topAnchor.constraint(equalTo: self.topAnchor, constant: self.frame.height * 0.063),
+            typeImage.widthAnchor.constraint(equalToConstant: size),
+            typeImage.heightAnchor.constraint(equalToConstant: size)
+        ])
+    }
+    
+    func setupTypeTitle() {
+        addSubview(typeTitle)
+        
+        let width = self.frame.width * 0.267
+        
+        NSLayoutConstraint.activate([
+            typeTitle.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            typeTitle.topAnchor.constraint(equalTo: typeImage.bottomAnchor, constant: self.frame.height * 0.073),
+            typeTitle.widthAnchor.constraint(equalToConstant: width),
+            typeTitle.heightAnchor.constraint(equalToConstant: width * 0.6)
+        ])
+    }
+    
+    func setupTypeDescription() {
+        addSubview(typeDescription)
+        
+        let width = self.frame.width * 0.756
+        
+        NSLayoutConstraint.activate([
+            typeDescription.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            typeDescription.topAnchor.constraint(equalTo: typeTitle.bottomAnchor, constant: self.frame.height * 0.073),
+            typeDescription.widthAnchor.constraint(equalToConstant: width),
+            typeDescription.heightAnchor.constraint(equalToConstant: width * 0.317)
+        ])
+    }
+    
+    func setupCheckMark() {
+        addSubview(checkMark)
+        
+        let size = self.frame.width * 0.111
+        let space = self.frame.width * 0.06
+        
+        NSLayoutConstraint.activate([
+            checkMark.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -space),
+            checkMark.topAnchor.constraint(equalTo: self.topAnchor, constant: space),
+            checkMark.widthAnchor.constraint(equalToConstant: size),
+            checkMark.heightAnchor.constraint(equalToConstant: size)
+        ])
+    }
+    
+    func onSelectedIdChange() {
+        if id == selectedId {
+            layer.borderWidth = 1
+            checkMark.isHidden = false
+        } else {
+            layer.borderWidth = 0
+            checkMark.isHidden = true
+        }
+    }
+    
+    @objc func onViewTap() {
+        delegate?.onCardSelect(cardId: id)
+    }
+    
+    
+}
