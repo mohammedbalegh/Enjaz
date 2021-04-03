7de143745aad19507595cf78a565671de25f7a9e
Mohammed Balegh <mohammed777balegh@gmail.com>
2021-04-03 21:32:15 +0200

    finished achievementsScreen

---
+++ Enjaz/ViewControllers/Screens/AchievementsScreenVC.swift
@@ -0,0 +1,137 @@
+import UIKit
+
+class AchievementsScreenVC: UIViewController {
+    
+    var achievements: [ItemModel] = [] {
+        didSet {
+            achievementsCarousel.reloadData()
+        }
+    }
+    
+    let noAchievementsLabel: UILabel = {
+        let label = UILabel()
+        label.text = NSLocalizedString("No achievements to show", comment: "")
+        label.textColor = .lightGray
+        label.font = .systemFont(ofSize: 18)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let achievementsCarousel: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.register(AchievementsCell.self, forCellWithReuseIdentifier: "achievementsCell")
+        cv.decelerationRate = UIScrollView.DecelerationRate.fast
+        cv.backgroundColor = .clear
+        cv.showsHorizontalScrollIndicator = false
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        achievementsCarousel.delegate = self
+        achievementsCarousel.dataSource = self
+        
+        setupSubViews()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        updateItemModels()
+        
+        if achievements.count == 0 {
+            noAchievementsLabel.isHidden = false
+        } else {
+            noAchievementsLabel.isHidden = true
+        }
+    }
+    
+    func setupSubViews() {
+        setupAchievementsCarousel()
+        setupNoAchievementsLabel()
+    }
+    
+    func setupNoAchievementsLabel() {
+        view.addSubview(noAchievementsLabel)
+        
+        NSLayoutConstraint.activate([
+            noAchievementsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            noAchievementsLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            noAchievementsLabel.widthAnchor.constraint(equalToConstant: LayoutConstants.screenWidth * 0.6),
+            noAchievementsLabel.heightAnchor.constraint(equalToConstant: 30)
+            
+        ])
+    }
+    
+    func setupAchievementsCarousel() {
+        view.addSubview(achievementsCarousel)
+        
+        NSLayoutConstraint.activate([
+            achievementsCarousel.topAnchor.constraint(equalTo: view.topAnchor, constant: LayoutConstants.screenHeight * 0.1),
+            achievementsCarousel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            achievementsCarousel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            achievementsCarousel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -(LayoutConstants.screenHeight * 0.131))
+        ])
+    }
+    
+    func getUpdatedAchievementsModel() -> [ItemModel] {
+        let itemModels = RealmManager.retrieveItems(withFilter: "type == 2")
+        
+        var updatedAchievements: [ItemModel] = []
+        
+        for item in itemModels {
+            updatedAchievements.append(item)
+        }
+        
+        return updatedAchievements
+    }
+    
+    func updateItemModels() {
+        let updatedAchievements = getUpdatedAchievementsModel()
+        
+        achievements = updatedAchievements
+    }
+    
+    func snapToCenter() {
+        let centerPoint = view.convert(view.center, to: achievementsCarousel)
+        guard let centerIndexPath = achievementsCarousel.indexPathForItem(at:centerPoint) else { return }
+        achievementsCarousel.scrollToItem(at: centerIndexPath, at: .centeredHorizontally, animated: true)
+    }
+
+}
+
+extension AchievementsScreenVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIScrollViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return achievements.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "achievementsCell", for: indexPath) as! AchievementsCell
+        cell.viewModel = achievements[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: LayoutConstants.screenWidth * 0.777, height: LayoutConstants.screenHeight * 0.564 )
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+
+        return UIEdgeInsets(top: 0, left: LayoutConstants.screenWidth * 0.111, bottom: 0, right: LayoutConstants.screenWidth * 0.111)
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        self.snapToCenter()
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        self.snapToCenter()
+    }
+
+}
---
+++ Enjaz/Views/AchievementsCardView.swift
@@ -0,0 +1,135 @@
+import UIKit
+
+class AchievementsCardView: UIView {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = label.font.withSize(17)
+        label.textColor = UIColor(hex: 0x011942)
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let categoryLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = label.font.withSize(14)
+        label.numberOfLines = 1
+        label.layer.borderColor = UIColor.gray.cgColor
+        label.layer.borderWidth = 0.5
+        label.layer.masksToBounds = true
+        label.textColor = .darkGray
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = label.font.withSize(13)
+        label.textAlignment = .center
+        label.textColor = .darkGray
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .gray
+        label.font = label.font.withSize(12)
+        label.adjustsFontSizeToFitWidth = true
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.textAlignment = .center
+        label.minimumScaleFactor = 0.1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func layoutSubviews() {
+         super.layoutSubviews()
+         self.backgroundColor = .white
+         self.layer.cornerRadius = 8
+     }
+    
+    override func didMoveToWindow() {
+        guard window != nil else { return }
+        guard superview != nil else { return }
+        
+        setupSubviews()
+    }
+    
+    func setupSubviews() {
+        setupTitleLabel()
+        setupCategoryLabel()
+        setupTimeLabel()
+        setupDescriptionLabel()
+    }
+    
+    func setupTitleLabel() {
+        addSubview(titleLabel)
+        
+        guard let superview = superview as? AchievementsCell else { return }
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            titleLabel.widthAnchor.constraint(equalTo: superview.widthAnchor, multiplier: 0.9),
+            titleLabel.heightAnchor.constraint(equalTo: superview.heightAnchor, multiplier: 0.1),
+            titleLabel.topAnchor.constraint(equalTo: superview.medalImage.bottomAnchor, constant: 20),
+        ])
+    }
+    
+    func setupCategoryLabel() {
+        addSubview(categoryLabel)
+        
+        let width = LayoutConstants.screenWidth * 0.208
+        let height = LayoutConstants.screenHeight * 0.025
+        
+        NSLayoutConstraint.activate([
+            categoryLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            categoryLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            categoryLabel.widthAnchor.constraint(equalToConstant: width),
+            categoryLabel.heightAnchor.constraint(equalToConstant: height)
+        ])
+        
+        categoryLabel.layer.cornerRadius = height / 2;
+    }
+    
+    func setupTimeLabel() {
+        addSubview(timeLabel)
+
+        
+        NSLayoutConstraint.activate([
+            timeLabel.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: 20),
+            timeLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            timeLabel.widthAnchor.constraint(equalToConstant: LayoutConstants.screenWidth * 0.168),
+            timeLabel.heightAnchor.constraint(equalToConstant: LayoutConstants.screenHeight * 0.0184),
+        ])
+    }
+    
+    func setupDescriptionLabel() {
+        addSubview(descriptionLabel)
+                
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 40),
+            descriptionLabel.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.9),
+            descriptionLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            descriptionLabel.heightAnchor.constraint(equalToConstant: 40),
+        ])
+    }
+    
+}
